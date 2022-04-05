6667ce7cddf7e844d64f291fec19fe818bbdfbc9
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2022-04-05 12:32:32 +0200

    💄 Set Poppins as default font

--- Projekt/markitdown/main.cpp
+++ Projekt/markitdown/main.cpp
@@ -1,15 +1,22 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include <QFont>
+#include <QFontDatabase>
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    for(auto& font: QDir(":/assets/fonts/Poppins").entryList(QStringList{"*.ttf"}, QDir::Files)){
+        QFontDatabase::addApplicationFont(":/assets/fonts/Poppins/" + font);
+    }
+
     app.setApplicationDisplayName("Mark It Down");
     app.setApplicationName("Mark It Down");
     app.setOrganizationName("polsl");
     app.setDesktopFileName("markitdown");
+    app.setFont(QFont("Poppins"));
 
     QQmlApplicationEngine engine;
     const QUrl url(u"qrc:/main.qml"_qs);
