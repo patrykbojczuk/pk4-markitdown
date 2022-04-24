5bde2e7c4e4a110cba28399e05cd38a6c4e612b4
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2022-04-24 14:04:11 +0200

    ✅ Add HeaderElement id-related tests

--- Projekt/markitdown/lib/src/markdown-parser/tests/src/markdown-document/markdown-elements/headerelement.cpp
+++ Projekt/markitdown/lib/src/markdown-parser/tests/src/markdown-document/markdown-elements/headerelement.cpp
@@ -28,3 +28,24 @@ TEST(HeaderElement, SetsTextLineElementOnConstruction) {
 
     ASSERT_STREQ(retStr.c_str(), L"Example text");
 }
+
+TEST(HeaderElement, ReturnsIdExistenceState){
+    VInlineMarkdownElement textElement1(make_recursive<PlainTextElement>(L"Example text"));
+    TextLineElement element1(textElement1);
+    HeaderElement header1(MarkdownHeaderLevel::Level5, element1);
+
+    VInlineMarkdownElement textElement2(make_recursive<PlainTextElement>(L"Another text"));
+    TextLineElement element2(textElement2);
+    HeaderElement header2(MarkdownHeaderLevel::Level5, element2, L"example_id");
+
+    EXPECT_FALSE(header1.hasId());
+    EXPECT_TRUE(header2.hasId());
+}
+
+TEST(HeaderElement, SetsEmptyIdIfNotDefined){
+    VInlineMarkdownElement textElement(make_recursive<PlainTextElement>(L"Example text"));
+    TextLineElement element(textElement);
+    HeaderElement header(MarkdownHeaderLevel::Level5, element);
+
+    EXPECT_STREQ(header.getId().c_str(), L"");
+}
