ecd520a43f8bc9d4c8d327b80986e9231b07d953
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2022-04-26 14:25:09 +0200

    ✨ Create LinkableReference

---
+++ Projekt/markitdown/lib/src/markdown-parser/linkablereference.h
@@ -0,0 +1,14 @@
+#ifndef LINKABLEREFERENCE_H
+#define LINKABLEREFERENCE_H
+#include <string>
+
+namespace MarkdownParser {
+    namespace MarkdownParser {
+        struct LinkableReference {
+            std::wstring url;
+            std::wstring title;
+        };
+    };
+};
+
+#endif // LINKABLEREFERENCE_H
