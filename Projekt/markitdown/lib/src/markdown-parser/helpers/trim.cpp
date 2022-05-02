57503b2d8214b18abdf7e5877c2480424e109fd3
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2022-05-02 13:26:00 +0200

    ✨ Create string_view trim helper functions
    Functions:
    - `rtrim` - trims from right
    - `ltrim` - trims from left
    - `trim` - trims from both sides
    - `wsv_*` - trims wstring_view

---
+++ Projekt/markitdown/lib/src/markdown-parser/helpers/trim.cpp
@@ -0,0 +1,41 @@
+#include <string_view>
+#include <iostream>
+
+using namespace std::string_view_literals;
+
+template<typename _CharT, typename _Traits = std::char_traits<_CharT>>
+auto rtrim(std::basic_string_view<_CharT, _Traits> sv,
+           std::basic_string<_CharT, _Traits> chars) {
+    auto lNO = sv.find_last_not_of(chars);
+    if (lNO != std::string_view::npos) {
+        sv.remove_suffix(std::min(sv.length() - lNO - 1, sv.length()));
+    }
+
+    return sv;
+}
+
+template<typename _CharT, typename _Traits = std::char_traits<_CharT>>
+auto ltrim(std::basic_string_view<_CharT, _Traits> sv,
+           std::basic_string<_CharT, _Traits> chars) {
+    sv.remove_prefix(std::min(sv.find_first_not_of(chars), sv.length()));
+
+    return sv;
+}
+
+template<typename _CharT, typename _Traits = std::char_traits<_CharT>>
+auto trim(std::basic_string_view<_CharT, _Traits> sv,
+          std::basic_string<_CharT, _Traits> chars) {
+    return rtrim(ltrim(sv, chars), chars);
+}
+
+auto wsv_rtrim(std::wstring_view sv, std::wstring chars = L"\t\n\v\f\r ") {
+    return rtrim(sv, chars);
+}
+
+auto wsv_ltrim(std::wstring_view sv, std::wstring chars = L"\t\n\v\f\r ") {
+    return ltrim(sv, chars);
+}
+
+auto wsv_trim(std::wstring_view sv, std::wstring chars = L"\t\n\v\f\r ") {
+    return trim(sv, chars);
+}
