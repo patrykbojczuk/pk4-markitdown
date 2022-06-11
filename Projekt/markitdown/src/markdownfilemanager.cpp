8a2470be3670a03522e67f578f21494f834ced36
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2022-06-11 10:49:55 +0200

    ✨ Create MarkdownFileManager

---
+++ Projekt/markitdown/src/markdownfilemanager.cpp
@@ -0,0 +1,40 @@
+#include "markdownfilemanager.h"
+#include "filemanager.h"
+#include "lib/src/markdown-parser/markdownparser.h"
+
+MarkdownFileManager::MarkdownFileManager() {}
+
+
+MarkdownFileManager &MarkdownFileManager::GetInstance()
+{
+    static MarkdownFileManager manager;
+    return manager;
+}
+
+QString MarkdownFileManager::open(const QString &filename)
+{
+    if (filename.contains("file://")) {
+        return FileManager::openTextFile(filename.sliced(7));
+    }
+
+    return FileManager::openTextFile(filename);
+}
+
+void MarkdownFileManager::save(const QString &filename, const QString &document)
+{
+    if (filename.contains("file://")) {
+        FileManager::saveTextFile(filename.sliced(7), document);
+        return;
+    }
+
+    FileManager::saveTextFile(filename, document);
+}
+
+void MarkdownFileManager::save(const QString &filename, MarkdownParser::MarkdownDocument::MarkdownDocument document, std::unique_ptr<IConverter> converter)
+{
+    if (filename.contains("file://")) {
+        MarkdownFileManager::save(filename.sliced(7), converter->convert(document));
+        return;
+    }
+    MarkdownFileManager::save(filename, converter->convert(document));
+}
---
+++ Projekt/markitdown/src/markdownfilemanager.h
@@ -0,0 +1,36 @@
+#ifndef MARKDOWNFILEMANAGER_H
+#define MARKDOWNFILEMANAGER_H
+
+#include <QObject>
+#include <QString>
+#include "converters/iconverter.h"
+#include "lib/src/markdown-parser/markdown-document/markdowndocument.h"
+
+class MarkdownFileManager : public QObject
+{
+    Q_OBJECT
+public:
+    static MarkdownFileManager &GetInstance();
+
+    Q_INVOKABLE
+    QString open(const QString &filename);
+
+    Q_INVOKABLE
+    void save(const QString &filename, const QString &document);
+
+    Q_INVOKABLE
+    template <typename T = IConverter>
+    void save(const QString &filename, MarkdownParser::MarkdownDocument::MarkdownDocument document);
+
+private:
+    MarkdownFileManager();
+    void save(const QString &filename, MarkdownParser::MarkdownDocument::MarkdownDocument document, std::unique_ptr<IConverter> converter);
+};
+
+template<typename T>
+void MarkdownFileManager::save(const QString &filename, MarkdownParser::MarkdownDocument::MarkdownDocument document)
+{
+    MarkdownFileManager::save(filename, document, std::move(std::unique_ptr<T>()));
+}
+
+#endif // MARKDOWNFILEMANAGER_H
