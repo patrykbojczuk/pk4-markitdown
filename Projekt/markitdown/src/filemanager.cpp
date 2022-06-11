3a05dea5e8fa56339992f1ca3d6ff745b39c8bc2
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2022-06-11 10:47:20 +0200

    ✨ Create FileManager class

---
+++ Projekt/markitdown/src/filemanager.cpp
@@ -0,0 +1,66 @@
+#include "filemanager.h"
+#include <QFile>
+#include <filesystem>
+
+FileManager::FileManager() {}
+
+QString FileManager::openTextFile(const QString &filename)
+{
+    QFile file(filename);
+    return FileManager::openTextFile(file);
+}
+
+QString FileManager::openTextFileOrCreate(const QString &filename, const QString &defaultValue)
+{
+    QFile file(filename);
+    try {
+        return FileManager::openTextFile(file);
+    } catch (...) {
+        saveTextFile(file, defaultValue);
+        file.close();
+        return defaultValue;
+    }
+}
+
+void FileManager::saveTextFile(const QString &filename, const QString &content)
+{
+    QFile file(filename);
+    saveTextFile(file, content);
+    file.close();
+}
+
+QString FileManager::openTextFile(QFile &file)
+{
+    if (fileExists(file.fileName())) {
+        if (file.open(QIODevice::ReadOnly)) {
+            auto configData = QString::fromUtf8(file.readAll());
+            file.close();
+            return configData;
+        }
+    }
+    file.close();
+    throw std::bad_exception();
+}
+
+void FileManager::saveTextFile(QFile &file, const QString &content)
+{
+    checkAndCreateParentPath(file.fileName());
+    if (file.open(QIODevice::WriteOnly)) {
+        file.write(content.toUtf8());
+        return;
+    }
+    file.close();
+    throw std::bad_exception();
+}
+
+bool FileManager::fileExists(const QString &filename) {
+    return std::filesystem::exists(filename.toStdString());
+}
+
+void FileManager::checkAndCreateParentPath(const QString &filename) {
+    auto parentPath = std::filesystem::path(filename.toStdString()).parent_path();
+    if (!std::filesystem::exists(parentPath)) {
+        std::filesystem::create_directories(parentPath);
+    }
+}
+
---
+++ Projekt/markitdown/src/filemanager.h
@@ -0,0 +1,25 @@
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
+
+#include <QString>
+#include <QFile>
+
+class FileManager
+{
+public:
+    FileManager();
+
+    static QString openTextFile(const QString &filename);
+    static QString openTextFileOrCreate(const QString &filename, const QString &defaultValue = "");
+    static void saveTextFile(const QString &filename, const QString &content);
+
+    static bool fileExists(const QString &filename);
+
+    static void checkAndCreateParentPath(const QString &filename);
+
+private:
+    static QString openTextFile(QFile &file);
+    static void saveTextFile(QFile &file, const QString &content);
+};
+
+#endif // FILEMANAGER_H
