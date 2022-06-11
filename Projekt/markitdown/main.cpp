35716b96590096d617f3fa57fb03b9af4544bf46
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2022-06-11 10:52:36 +0200

    ✨ Register Managers' instances for QML

--- Projekt/markitdown/main.cpp
+++ Projekt/markitdown/main.cpp
@@ -5,12 +5,16 @@
 #include <QDir>
 #include <QQuickStyle>
 #include "src/backendconnection.h"
+#include "src/configmanager.h"
+#include "src/markdownfilemanager.h"
+#include "src/tabmanager.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    for (auto& font: QDir(":/assets/fonts/Poppins").entryList(QStringList{"*.ttf"}, QDir::Files)) {
+    for (auto &font : QDir(":/assets/fonts/Poppins").entryList(QStringList{"*.ttf"}, QDir::Files))
+    {
         QFontDatabase::addApplicationFont(":/assets/fonts/Poppins/" + font);
     }
 
@@ -24,14 +28,18 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
 
-    qmlRegisterSingletonInstance("pb.pk.markitdown", 1, 0, "BackendConnection", &BackendConnection::getInstance());
+    qmlRegisterSingletonInstance<ConfigManager>("pb.pk.markitdown", 1, 0, "ConfigManager", &ConfigManager::GetInstance());
+    qmlRegisterSingletonInstance<TabManager>("pb.pk.markitdown", 1, 0, "TabManager", &TabManager::GetInstance());
+    qmlRegisterSingletonInstance<MarkdownFileManager>("pb.pk.markitdown", 1, 0, "MarkdownFileManager", &MarkdownFileManager::GetInstance());
 
     const QUrl url(u"qrc:/main.qml"_qs);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
+    QObject::connect(
+        &engine, &QQmlApplicationEngine::objectCreated,
+        &app, [url](QObject *obj, const QUrl &objUrl)
+        {
         if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
+            QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
     engine.load(url);
 
     return app.exec();
