54e2a9d1512f801200f2a52ab6d63f3fc1945d77
Patryk Bojczuk <patryk@bojczuk.mail.pl>
2022-06-12 12:50:26 +0200

    ✨ Make ConfigManager's properties QML-available

--- Projekt/markitdown/src/configmanager.cpp
+++ Projekt/markitdown/src/configmanager.cpp
@@ -2,6 +2,7 @@
 #include <QJsonDocument>
 #include <QJsonArray>
 #include <QJsonObject>
+#include "tabmanager.h"
 
 ConfigManager &ConfigManager::GetInstance()
 {
@@ -9,85 +10,150 @@ ConfigManager &ConfigManager::GetInstance()
     return manager;
 }
 
-unsigned short ConfigManager::getFontSize() const
+unsigned short ConfigManager::fontSize() const
 {
-    return fontSize;
+    return m_fontSize;
 }
 
-void ConfigManager::setFontSize(unsigned short newFontSize)
+const QFont &ConfigManager::fontFamily() const
 {
-    fontSize = newFontSize;
-    save();
+    return m_fontFamily;
 }
 
-void ConfigManager::updateFontFamily(const QFont &newFontFamily)
+const QStringList &ConfigManager::recentFiles() const
 {
-    fontFamily = newFontFamily;
-    save();
-}
-
-const QStringList &ConfigManager::getRecentFiles() const
-{
-    return recentFiles;
-}
-
-void ConfigManager::addRecentFile(const QString &newRecentFile)
-{
-    recentFiles.push_back(newRecentFile);
-    save();
-}
-
-void ConfigManager::setAutosave(bool newAutosave)
-{
-    autosave = newAutosave;
-    save();
-}
-
-void ConfigManager::save()
-{
-    QJsonObject obj;
-    obj.insert("fontSize", fontSize);
-    obj.insert("fontName", fontFamily.family());
-    obj.insert("autosave", autosave);
-    obj.insert("recentFiles", QJsonValue(QJsonArray::fromStringList(recentFiles)));
-    QJsonDocument doc;
-    doc.setObject(obj);
-    configFileManager.saveConfig(doc.toJson(QJsonDocument::JsonFormat::Compact));
+    return m_recentFiles;
 }
 
 bool ConfigManager::isAutosaveEnabled() const
 {
-    return autosave;
+    return m_autosave;
+}
+
+size_t ConfigManager::autosaveTimeout() const
+{
+    return m_autosaveTimeout;
+}
+
+
+void ConfigManager::setFontSize(unsigned short newFontSize, bool save)
+{
+    m_fontSize = newFontSize;
+    if (save) {
+        saveConfig();
+    }
+    emit fontSizeChanged();
+}
+
+void ConfigManager::setFontFamily(const QFont &newFontFamily, bool save)
+{
+    m_fontFamily = newFontFamily;
+    if (save) {
+        saveConfig();
+    }
+    emit fontFamilyChanged();
+}
+
+void ConfigManager::addRecentFile(const QString &newRecentFile, bool save)
+{
+    m_recentFiles.push_back(newRecentFile);
+    if (save) {
+        saveConfig();
+    }
+    emit recentFilesChanged();
+}
+
+void ConfigManager::setAutosave(bool newAutosave, bool save)
+{
+    if (!m_autosave && newAutosave){
+        QMetaObject::invokeMethod(&autosaveTimer, [this](){
+            autosaveTimer.start(m_autosave);
+        }, Qt::QueuedConnection);
+        //autosaveTimer.start(5000);
+    }
+    if (!newAutosave){
+        QMetaObject::invokeMethod(&autosaveTimer, [this](){
+            autosaveTimer.stop();
+        }, Qt::QueuedConnection);
+        //autosaveTimer.stop();
+    }
+    m_autosave = newAutosave;
+    if (save) {
+        saveConfig();
+    }
+    emit autosaveChanged();
+}
+
+void ConfigManager::setAutosaveTimeout(size_t newAutosaveTimeout, bool save)
+{
+    if (m_autosave){
+        autosaveTimer.setInterval(newAutosaveTimeout);
+    }
+    m_autosaveTimeout = newAutosaveTimeout;
+    if (save) {
+        saveConfig();
+    }
+    emit autosaveTimeoutChanged();
+}
+
+void ConfigManager::saveConfig()
+{
+    QJsonObject obj;
+    obj.insert("fontSize", m_fontSize);
+    obj.insert("fontName", m_fontFamily.family());
+    obj.insert("autosave", m_autosave);
+    obj.insert("recentFiles", QJsonValue(QJsonArray::fromStringList(m_recentFiles)));
+    QJsonDocument doc;
+    doc.setObject(obj);
+    configFileManager.saveConfig(doc.toJson(QJsonDocument::JsonFormat::Compact));
 }
 
 const QString defaultConfig = "{\n"
                               "    \"fontSize\":12,\n"
                               "    \"fontName\":\"Poppins\",\n"
                               "    \"autosave\":true,\n"
+                              "    \"autosaveInterval_ms\":5000,\n"
                               "    \"recentFiles\":[]\n"
                               "}";
 
 void ConfigManager::loadConfig()
 {
-    QString config = configFileManager.openConfig(defaultConfig);
-    auto document = QJsonDocument::fromJson(config.toUtf8());
+    configFileManager.openConfig(defaultConfig)
+        .then([this](const QString& config){
+            auto document = QJsonDocument::fromJson(config.toUtf8());
 
-    fontSize = document["fontSize"].toInteger(12);
-    QString fontName = document["fontName"].toString("Poppins");
-    QFont font;
-    font.setFamily(fontName);
-    autosave = document["autosave"].toBool(true);
-    if (document["recentFiles"].isArray()) {
-        auto recentFilesArray = document["recentFiles"].toArray();
-        for (const auto& file: recentFilesArray) {
-            if (file.isString() && FileManager::fileExists(file.toString())) {
-                recentFiles.push_back(file.toString());
+            setFontSize(document["fontSize"].toInteger(12), false);
+            QString fontName = document["fontName"].toString("Poppins");
+            QFont fontFamily;
+            fontFamily.setFamily(fontName);
+            setFontFamily(fontFamily, false);
+            setAutosave(document["autosave"].toBool(true), false);
+            setAutosaveTimeout(document["autosaveInterval_ms"].toInteger(5000), false);
+            if (document["recentFiles"].isArray()) {
+                auto recentFilesArray = document["recentFiles"].toArray();
+                for (const auto& file: recentFilesArray) {
+                    if (file.isString() && FileManager::fileExists(file.toString())) {
+                        addRecentFile(file.toString(), false);
+                    }
+                }
             }
-        }
-    }
+
+            finishedLoading = true;
+    });
 }
 
-ConfigManager::ConfigManager(QObject *parent): QObject(parent)
+void ConfigManager::autosaveElapsed()
+{
+    TabManager::GetInstance().saveAll();
+}
+
+bool ConfigManager::getFinishedLoading() const
+{
+    return finishedLoading;
+}
+
+ConfigManager::ConfigManager(QObject *parent): QObject(parent), autosaveTimer(this)
 {
     loadConfig();
+    connect(&autosaveTimer, &QTimer::timeout, this, QOverload<>::of(&ConfigManager::autosaveElapsed));
 }
--- Projekt/markitdown/src/configmanager.h
+++ Projekt/markitdown/src/configmanager.h
@@ -3,37 +3,61 @@
 
 #include <QObject>
 #include <QtQml/qqml.h>
-#include "QtGui/qfont.h"
+#include <QFont>
+#include <QTimer>
 #include "configfilemanager.h"
 
 class ConfigManager : public QObject
 {
     Q_OBJECT
-
+    Q_PROPERTY(unsigned short fontSize READ fontSize WRITE setFontSize NOTIFY fontSizeChanged)
+    Q_PROPERTY(QFont fontFamily READ fontFamily WRITE setFontFamily NOTIFY fontFamilyChanged)
+    Q_PROPERTY(QStringList recentFiles READ recentFiles NOTIFY recentFilesChanged)
+    Q_PROPERTY(bool autosave READ isAutosaveEnabled WRITE setAutosave NOTIFY autosaveChanged)
+    Q_PROPERTY(size_t autosaveTimeout READ autosaveTimeout WRITE setAutosaveTimeout NOTIFY autosaveTimeoutChanged)
 public:
     static ConfigManager &GetInstance();
 
-    unsigned short getFontSize() const;
-    QFont &getFontFamily() const;
-    const QStringList &getRecentFiles() const;
+    unsigned short fontSize() const;
+    const QFont &fontFamily() const;
+    const QStringList &recentFiles() const;
     bool isAutosaveEnabled() const;
 
-    void setFontSize(unsigned short newFontSize);
-    void updateFontFamily(const QFont &newFontFamily);
-    void addRecentFile(const QString &newRecentFile);
-    void setAutosave(bool newAutosave);
+    size_t autosaveTimeout() const;
+
+    void setFontSize(unsigned short newFontSize, bool save = true);
+    void setFontFamily(const QFont &newFontFamily, bool save = true);
+    Q_INVOKABLE
+    void addRecentFile(const QString &newRecentFile, bool save = true);
+    void setAutosave(bool newAutosave, bool save = true);
+    void setAutosaveTimeout(size_t newAutosaveTimeout, bool save = true);
+
+    bool getFinishedLoading() const;
+
+signals:
+    void fontSizeChanged();
+    void fontFamilyChanged();
+    void recentFilesChanged();
+    void autosaveChanged();
+    void autosaveTimeoutChanged();
 
 private:
-    QFont fontFamily;
-    unsigned short fontSize;
-    QStringList recentFiles;
-    bool autosave;
+    QFont m_fontFamily;
+    unsigned short m_fontSize;
+    QStringList m_recentFiles;
+    bool m_autosave;
+    size_t m_autosaveTimeout;
+    QTimer autosaveTimer;
 
     ConfigFileManager configFileManager;
-    void save();
+    void saveConfig();
 
     ConfigManager(QObject *parent = nullptr);
     void loadConfig();
+
+    void autosaveElapsed();
+
+    bool finishedLoading = false;
 };
 
 #endif // CONFIGMANAGER_H
